ca178ec2cae1e6b58492035406c14402762e7354
Jlia904 <Jubnliang@gmail.com>
2023-02-11 22:39:13 +1300

    new dataset + train with updates

--- Dynamixel_motors/MPX5010_pressure_sensor_read_python/MPX5010_pressure_sensor_read_python.ino
+++ Dynamixel_motors/MPX5010_pressure_sensor_read_python/MPX5010_pressure_sensor_read_python.ino
@@ -20,12 +20,38 @@ void setup() {
     pressure_1 = pressure_1 * (1-alpha) + alpha *analogRead(A0);
     pressure_2 = pressure_2 * (1-alpha) + alpha *analogRead(A1);
   }
-
+  
   pressure_1_tare = pressure_1;
   pressure_2_tare = pressure_2;
   digitalWrite(13, HIGH);  
 
 }
+
+void send_data(float data_1, float data_2){
+  if (data_1 < 100){
+      if (data_1 < 10){
+        Serial.print("00");
+      }
+      else{
+        Serial.print("0");     
+      }
+    }
+
+    Serial.print(data_1, 2);
+    Serial.print(",");
+
+    if (data_2 < 100){
+      if (data_2 < 10){
+        Serial.print("00");
+      }
+      else{
+        Serial.print("0");
+      }
+    }
+    
+    Serial.println(data_2, 2);  
+}
+
 
 void loop() {
 
@@ -35,19 +61,22 @@ void loop() {
     digitalWrite(13, HIGH);    
     pressure_1_tare = pressure_1;
     pressure_2_tare = pressure_2;
+
+    send_data(pressure_1 - pressure_1_tare, pressure_2 - pressure_2_tare);
+    
   }
   else if (incoming_data == '0'){
     digitalWrite(13, LOW);    
     pressure_1_tare = 0;
     pressure_2_tare = 0;
-    Serial.print(pressure_1 - pressure_1_tare);
-    Serial.print(",");
-    Serial.println(pressure_2 - pressure_2_tare);    
+    
+    send_data(pressure_1 - pressure_1_tare, pressure_2 - pressure_2_tare);
+    
   }
-  else if (incoming_data == '2'){
-    Serial.print(pressure_1 - pressure_1_tare);
-    Serial.print(",");
-    Serial.println(pressure_2 - pressure_2_tare);
+  else if (incoming_data == '2') {
+    digitalWrite(13, HIGH);
+    send_data(pressure_1 - pressure_1_tare, pressure_2 - pressure_2_tare);
+    
   }
   
   pressure_1 = pressure_1 * (1-alpha) + alpha *analogRead(A0);
--- Dynamixel_motors/Python_serial_communication_test/Arduino_serial_communication_test.py
+++ Dynamixel_motors/Python_serial_communication_test/Arduino_serial_communication_test.py
@@ -5,12 +5,14 @@ ser = serial.Serial('/dev/ttyACM0', 57600, timeout=1)
 
 time.sleep(1)
 ser.read(100)
-ser.write(b'1')
+# ser.write(b'1')
 
-for i in range(0, 100):
+for i in range(0, 2):
     # time.sleep(0.1)
     ser.write(b'2')
-    x = ser.read(13)
+    time.sleep(0.1)
+    x = ser.read(15)
     print(x)
 
 ser.close()
+
