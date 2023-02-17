ff137b92c814892507fa70dc5dd5ce781b2e5a88
Jlia904 <Jubnliang@gmail.com>
2023-02-17 20:03:20 +1300

    Added NN model for classification

--- Dynamixel_motors/Python_serial_communication_test/Arduino_serial_communication_test.py
+++ Dynamixel_motors/Python_serial_communication_test/Arduino_serial_communication_test.py
@@ -15,4 +15,3 @@ for i in range(0, 2):
     print(x)
 
 ser.close()
-pd.Series(kirigami_area_arr), pd.Series(kirigami_perimeter_arr), pd.Series(kirigami_width_arr), pd.Series(kirigami_height_arr), pd.Series(kirigami_aspect_ratio_arr), pd.Series(hole_area_arr), pd.Series(hole_perimeter_arr), pd.Series(hole_width_arr), pd.Series(hole_height_arr), pd.Series(hole_aspect_ratio_arr)
