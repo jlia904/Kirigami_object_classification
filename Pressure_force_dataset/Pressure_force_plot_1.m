f764627ba1dbcc0c9e18250499573c0a25df1b69
Jlia904 <Jubnliang@gmail.com>
2023-03-05 21:58:09 +1300

    Progress save submitted IROS paper

--- Pressure_force_dataset/Pressure_force_plot_1.m
+++ Pressure_force_dataset/Pressure_force_plot_1.m
@@ -6,21 +6,28 @@ curved_plate_40mm_dir = strcat(working_dir, '/Curved_plate/C_distance_40mm_cycle
 curved_plate_40mm_data = readtable(curved_plate_40mm_dir);
 
 
-scatter(curved_plate_40mm_data.pressure_reading_1, curved_plate_40mm_data.weight_reading_1/1000*9.81, 15, 'o', 'MarkerEdgeColor', '#D95319')
+scatter(curved_plate_40mm_data.weight_reading_1/1000*9.81, curved_plate_40mm_data.pressure_reading_1, 15, 'o', 'MarkerEdgeColor', '#D95319')
 hold on
 
 
 flat_plate_40mm_dir = strcat(working_dir, '/Flat_plate/F_distance_40mm_cycle.csv');
 flat_plate_40mm_data = readtable(flat_plate_40mm_dir);
 
-
-
-scatter(flat_plate_40mm_data.pressure_reading_1, flat_plate_40mm_data.weight_reading_1/1000*9.81, 15, 'o', 'MarkerEdgeColor', '#0072BD')
+scatter(flat_plate_40mm_data.weight_reading_1/1000*9.81, flat_plate_40mm_data.pressure_reading_1, 15, 'o', 'MarkerEdgeColor', '#0072BD')
 hold on
 
-xlabel("Raw Pressure Reading")
-ylabel("Force (N)")
-lgd = legend('Curved Plate 40 mm', 'Flat Plate 40 mm');
-ylim([0, 2.1])
+ylabel("Raw Pressure Reading")
+xlabel("Force (N)")
 
+xlim([0, 2.1])
+
+
+
+x = 0:0.1:2.1;
+curved_fit = -12.6*x.^3+42.27*x.^2+15.93.*x+0.8248;
+flat_fit = -3.47*x.^3+20.3*x.^2+15.35.*x+0.6163;
+plot(x,curved_fit, 'LineWidth', 2, 'Color', '#77AC30');
+hold on
+plot(x, flat_fit, 'LineWidth', 2, 'Color', '#EDB120')
+lgd = legend('Curved Plate 40 mm', 'Flat Plate 40 mm', 'y=-12.6x^3+42.3x^2+15.9x+0.8', 'y=-3.5x^3+20.3x^2+15.4x+0.6');
 lgd.FontSize = 11;
--- Pressure_force_dataset/Pressure_force_plot_2.m
+++ Pressure_force_dataset/Pressure_force_plot_2.m
@@ -1,42 +1,42 @@
 figure;
 
 working_dir = pwd;
-curved_plate_20mm_dir = strcat(working_dir, '\Curved_plate\C_distance_20mm_combined.csv');
+curved_plate_20mm_dir = strcat(working_dir, '/Curved_plate/C_distance_20mm_combined.csv');
 curved_plate_20mm_data = readtable(curved_plate_20mm_dir);
-curved_plate_30mm_dir = strcat(working_dir, '\Curved_plate\C_distance_30mm_combined.csv');
+curved_plate_30mm_dir = strcat(working_dir, '/Curved_plate/C_distance_30mm_combined.csv');
 curved_plate_30mm_data = readtable(curved_plate_30mm_dir);
-curved_plate_40mm_dir = strcat(working_dir, '\Curved_plate\C_distance_40mm_combined.csv');
+curved_plate_40mm_dir = strcat(working_dir, '/Curved_plate/C_distance_40mm_combined.csv');
 curved_plate_40mm_data = readtable(curved_plate_40mm_dir);
-curved_plate_50mm_dir = strcat(working_dir, '\Curved_plate\C_distance_50mm_combined.csv');
+curved_plate_50mm_dir = strcat(working_dir, '/Curved_plate/C_distance_50mm_combined.csv');
 curved_plate_50mm_data = readtable(curved_plate_50mm_dir);
 
-scatter(curved_plate_20mm_data.pressure_reading_1, curved_plate_20mm_data.weight_reading_1/1000*9.81, 15, 'o', 'MarkerEdgeColor', '#77AC30')
+scatter(curved_plate_20mm_data.weight_reading_1/1000*9.81, curved_plate_20mm_data.pressure_reading_1, 15, 'o', 'MarkerEdgeColor', '#77AC30')
 hold on
-scatter(curved_plate_30mm_data.pressure_reading_1, curved_plate_30mm_data.weight_reading_1/1000*9.81, 15, 'o', 'MarkerEdgeColor', '#EDB120')
+scatter(curved_plate_30mm_data.weight_reading_1/1000*9.81, curved_plate_30mm_data.pressure_reading_1, 15, 'o', 'MarkerEdgeColor', '#EDB120')
 hold on
-scatter(curved_plate_40mm_data.pressure_reading_1, curved_plate_40mm_data.weight_reading_1/1000*9.81, 15, 'o', 'MarkerEdgeColor', '#D95319')
+scatter(curved_plate_40mm_data.weight_reading_1/1000*9.81, curved_plate_40mm_data.pressure_reading_1, 15, 'o', 'MarkerEdgeColor', '#D95319')
 hold on
-scatter(curved_plate_50mm_data.pressure_reading_1, curved_plate_50mm_data.weight_reading_1/1000*9.81, 15, 'o', 'MarkerEdgeColor', '#0072BD')
+scatter(curved_plate_50mm_data.weight_reading_1/1000*9.81, curved_plate_50mm_data.pressure_reading_1, 15, 'o', 'MarkerEdgeColor', '#0072BD')
 
-flat_plate_20mm_dir = strcat(working_dir, '\Flat_plate\F_distance_20mm_combined.csv');
+flat_plate_20mm_dir = strcat(working_dir, '/Flat_plate/F_distance_20mm_combined.csv');
 flat_plate_20mm_data = readtable(flat_plate_20mm_dir);
-flat_plate_30mm_dir = strcat(working_dir, '\Flat_plate\F_distance_30mm_combined.csv');
+flat_plate_30mm_dir = strcat(working_dir, '/Flat_plate/F_distance_30mm_combined.csv');
 flat_plate_30mm_data = readtable(flat_plate_30mm_dir);
-flat_plate_40mm_dir = strcat(working_dir, '\Flat_plate\F_distance_40mm_combined.csv');
+flat_plate_40mm_dir = strcat(working_dir, '/Flat_plate/F_distance_40mm_combined.csv');
 flat_plate_40mm_data = readtable(flat_plate_40mm_dir);
-flat_plate_50mm_dir = strcat(working_dir, '\Flat_plate\F_distance_50mm_combined.csv');
+flat_plate_50mm_dir = strcat(working_dir, '/Flat_plate/F_distance_50mm_combined.csv');
 flat_plate_50mm_data = readtable(flat_plate_50mm_dir);
 
-scatter(flat_plate_20mm_data.pressure_reading_1, flat_plate_20mm_data.weight_reading_1/1000*9.81, 15, '^', 'MarkerEdgeColor', '#77AC30')
+scatter(flat_plate_20mm_data.weight_reading_1/1000*9.81, flat_plate_20mm_data.pressure_reading_1, 15, '^', 'MarkerEdgeColor', '#77AC30')
 hold on
-scatter(flat_plate_30mm_data.pressure_reading_1, flat_plate_30mm_data.weight_reading_1/1000*9.81, 15, '^', 'MarkerEdgeColor', '#EDB120')
+scatter(flat_plate_30mm_data.weight_reading_1/1000*9.81, flat_plate_30mm_data.pressure_reading_1, 15, '^', 'MarkerEdgeColor', '#EDB120')
 hold on
-scatter(flat_plate_40mm_data.pressure_reading_1, flat_plate_40mm_data.weight_reading_1/1000*9.81, 15, '^', 'MarkerEdgeColor', '#D95319')
+scatter(flat_plate_40mm_data.weight_reading_1/1000*9.81, flat_plate_40mm_data.pressure_reading_1, 15, '^', 'MarkerEdgeColor', '#D95319')
 hold on
-scatter(flat_plate_50mm_data.pressure_reading_1, flat_plate_50mm_data.weight_reading_1/1000*9.81, 15, '^', 'MarkerEdgeColor', '#0072BD')
+scatter(flat_plate_50mm_data.weight_reading_1/1000*9.81, flat_plate_50mm_data.pressure_reading_1, 15, '^', 'MarkerEdgeColor', '#0072BD')
 hold on
 
-xlabel("Raw Pressure Reading")
-ylabel("Force (N)")
+ylabel("Raw Pressure Reading")
+xlabel("Force (N)")
 lgd = legend('Curved Plate 20 mm', 'Curved Plate 30 mm', 'Curved Plate 40 mm', 'Curved Plate 50 mm', 'Flat Plate 20 mm', 'Flat Plate 30 mm', 'Flat Plate 40 mm', 'Flat Plate 50 mm');
 lgd.FontSize = 10;
